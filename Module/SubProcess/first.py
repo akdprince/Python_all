622e91fa07ede4bfae375b5a58272b6a7f41db0a
akdprince <anik@Cuseng01.palonet.org>
2026-08-09 00:12:42 +0600

    subprocess added

---
+++ Module/SubProcess/first.py
@@ -0,0 +1,20 @@
+import subprocess
+
+# subprocess.run("ls -la", shell=True) #first process
+# p1 =  subprocess.run(["ls", "-la"]) #second process
+# print(p1)
+
+#if i want to capture the output of the command and provide to another file 
+
+# with open("/home/anik/lab/python/Module/SubProcess/output.txt", "w") as f:
+#     p1 = subprocess.run(["ls", "-la"], stdout=f, text=True) #third process
+
+# Show result from the first output file to the second output file and check data with input from the first output file
+
+p1 = subprocess.run(["cat", "/home/anik/lab/python/Module/SubProcess/output.txt"], capture_output=True, text=True) # Capture output in p1
+
+# print(p1.stdout)  # Print the captured output
+
+p2 = subprocess.run(["grep", "-n", "anik"], capture_output=True, text=True, input=p1.stdout)  # Use p1's output as input for p2
+
+print(p2.stdout)  # Print the output of the second process
