bbd8db0d80f3aee12bdd2548c35e93978a75c54e
anik <akd.prince07@gmail.com>
2026-08-09 11:47:03 +0600

    shutill added

---
+++ Module/Shutil/Shutil.py
@@ -0,0 +1,28 @@
+import shutil
+
+# # Copy a single file preserving metadata
+# shutil.copy2("/home/anik/python/Python_all/Module/Shutil/source.txt", "/home/anik/python/Python_all/Module/Shutil/backup_source.txt")
+
+# # Copy an entire folder and its contents
+# shutil.copytree("/home/anik/python/Python_all/Module/Shutil/Test", "/home/anik/python/Python_all/Module/Shutil/Test2")
+
+# # Move a file or directory to a new location
+# shutil.move("/home/anik/python/Python_all/Module/Shutil/Test/hello.txt", "/home/anik/python/Python_all/Module/Shutil/Test2/")
+
+# # Delete a non-empty directory and all subfiles
+# shutil.rmtree("/home/anik/python/Python_all/Module/Shutil/Test")
+
+#Make a folder zip file
+# shutil.make_archive("/home/anik/python/Python_all/Module/Shutil/backup", "zip", "/home/anik/python/Python_all/Module/Shutil/Test2")
+
+#extract a zip file to a specified directory
+# shutil.unpack_archive("/home/anik/python/Python_all/Module/Shutil/backup.zip", "/home/anik/python/Python_all/Module/Shutil/Test3")
+
+
+# Check disk space on current drive
+total, used, free = shutil.disk_usage(".")
+print(f"Total free Space: {free}")
+print(f"Free space: {free // (2**30)} GB") # 2**30 means 2 to the power of 30, which is the number of bytes in a gigabyte.
+    
+# Find where python or git executable is located
+print(shutil.which("git"))  # e.g., '/usr/bin/git'
