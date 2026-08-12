579499bddaa03f3fe16fc3cb600d6cf218d1b9b7
akdprince <anik@Cuseng01.palonet.org>
2026-08-12 10:12:27 +0600

    json added with api

--- json/First.py
+++ json/First.py
@@ -6,29 +6,39 @@ MyFiles = {
         {
             "name": "Anik",
             "age": 29,
-            "city": "Dhaka"
+            "city": "Dhaka",
+            "country": True
 
          },
          {
              "name": "Krishna",
              "age": 5100,
-             "city": "Vrindavan"
+             "city": "Vrindavan",
+             "country": False
          },
          {
                 "name": "Rama",
                 "age": 1000050,
-                "city": "Ayodhya"
+                "city": "Ayodhya",
+                "country": False
          },
          {
              "name": "Sita",
              "age": 1000000,
-             "city": "Mithila"
+             "city": "Mithila",
+             "country": False
          }
     ]
 
 }
 
+
 json_string = json.dumps(MyFiles, indent=4)
 
-with open("/home/anik/lab/python/json/MyFiles.json", "w") as f:
-    f.write(json_string)
+# with open("/home/anik/lab/python/json/MyFiles.json", "w") as f:
+#     f.write(json_string)
+
+data = json.loads(json_string)
+
+for person in data['people']:
+    print(person['name'])
